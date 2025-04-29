156c5ad30933ee22de495a894c7d220ef1b44d6b
Cadabra1999 <cadabrar@gmail.com>
2025-04-29 22:58:55 +0300

    app.py

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
-# Ecsc
-Ecsc
+from flask import Flask, request, jsonify
+import base64, io
+from PIL import Image
+import pytesseract
+
+app = Flask(__name__)
+
+@app.route('/image', methods=['POST'])
+def solve_image():
+    data = request.get_json()
+    base64_str = data['base64_string']
+    image_data = base64.b64decode(base64_str)
+    image = Image.open(io.BytesIO(image_data))
+    text = pytesseract.image_to_string(image, config='--psm 7 digits')
+    try:
+        result = str(eval(text.strip()))
+    except:
+        result = "?"
+    return jsonify({"solves": list(result)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
